c42a2d0ba90210ef97f179e9e2b2f5481f35551e
Eric Su <esericsu@gmail.com>
2020-10-30 12:21:21 -0700

    add button and fucntion to generate a 120X120 black image

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      number:[],
+      numbers:[],
     };
     this.getNumber = this.getNumber.bind(this);
   }
@@ -24,12 +24,37 @@ class App extends React.Component {
     }
     random.generateIntegers(options)
       .then((result) => {
-        console.log(result.random.data)
+        
+        this.setState({
+          numbers: result.random.data,
+        })
+        console.log(this.state.numbers)
       })
   }
 
-  createImage(num1, num2, num3) {
-
+  createImage(arr) {
+    const random = new RandomOrg({ apiKey: '7188c32e-e68e-4c2f-8539-ad019c187edc' });
+    const options = {
+      n: 3,
+      min: 0,
+      max: 255,
+    };
+    random.generateIntegers(options)
+      .then((result) => {
+        this.setState({
+          numbers: result.random.data,
+        })
+        console.log(this.state.numbers)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    const rgbString = `rgb(${arr[0], arr[1], arr[2], 1})`;
+    console.log(rgbString)
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = rgbString;
+    ctx.fillRect(10, 10, 120, 120);
   }
 
   handleClick() {
@@ -42,7 +67,10 @@ class App extends React.Component {
       <div>
         <h1>Generate RGB Image</h1>
         <button type="button" onClick={this.getNumber}>generate number</button>
-        <div></div>
+        <button type="button" onClick={this.createImage}>Create Image</button>
+        <div>
+          Image
+        </div>
       </div>
     );
   }
